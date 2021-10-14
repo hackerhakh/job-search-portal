d102c71e662f59c15b47c7098ed7c36c6b9ab425
hackerhakh <akhil.azeus@gmail.com>
2021-10-14 17:51:13 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div id="app"></div>
+  <div v-for="(job,index) in jobs"  :key="index">
+    <Job v-bind:title=job.title v-bind:location=job.location v-bind:description=job.description />
+  </div>
+</template>
+
+<script>
+import Job from './components/Job.vue'
+export default {
+  name: 'App',
+  components: {
+    Job
+  },
+  data(){
+    return{
+      jobs: [ {title:"Software Engineer", location:"India", description:"Design, develop and debug software applications"},
+      {title:"Software Engineer", location:"India", description:"Design, develop and debug software applications"} ]
+    }
+  }
+}
+</script>
+
+<style>
+#app{
+  background-color:whitesmoke;
+  margin:0px;
+  padding:0px;
+}
+
+</style>
